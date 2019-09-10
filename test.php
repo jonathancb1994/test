52b09c519258b1a96438f73c16b472cd811a73b4
jonathan390 <46784354+jonathan390@users.noreply.github.com>
2019-09-10 16:34:34 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,16 +3,16 @@
 for($i=1;$i<=100;$i++){
     switch($i){
         case($i%3==0 && $i%5==0) :
-            echo "Linianos"."<br>";
+            echo "Linianos"."<br>"; /* For multiples of 3 and 5*/
             break;        
         case($i%3==0):
-            echo "lino"."<br>";
+            echo "lino"."<br>"; /* For multiples of 3 */
             break;
         case($i%5==0) :
-            echo "IT"."<br>";
+            echo "IT"."<br>"; /* For multiples of 3 */
             break;
         default:
-            echo $i."<br>";
+            echo $i."<br>"; /* For all non mutiples of 3 and 5 */
             break;         
     }
 }
